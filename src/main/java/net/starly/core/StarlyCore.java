022e3b584480faf8653b3cf3903a99cd98b78dbf
yejunho10 <hyjcompany30@naver.com>
2023-01-02 16:00:19 +0900

    modify: version

--- src/main/java/net/starly/core/StarlyCore.java
+++ src/main/java/net/starly/core/StarlyCore.java
@@ -1,14 +1,7 @@
 package net.starly.core;
 
-import net.starly.core.bstats.Metrics;
-import net.starly.core.data.Config;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StarlyCore extends JavaPlugin {
-    public Config config;
-
-    @Override
-    public void onEnable() {
-        Metrics metrics = new Metrics(this, 17172);
-    }
+    @Override public void onEnable() {}
 }
