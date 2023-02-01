56e529c78d56596ecb42fc50622d6abf5b32ca48
yejunho10 <hyjcompany30@naver.com>
2023-02-01 16:35:27 +0900

    feat: Metrics

--- src/main/java/net/starly/core/StarlyCore.java
+++ src/main/java/net/starly/core/StarlyCore.java
@@ -1,7 +1,11 @@
 package net.starly.core;
 
+import net.starly.core.bstats.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StarlyCore extends JavaPlugin {
-    @Override public void onEnable() {}
+    @Override
+    public void onEnable() {
+        new Metrics(this, 17172);
+    }
 }
