0eed4cb06459c0fbdc072a3d525e393ae3e720a2
yejunho10 <hyjcompany30@naver.com>
2023-02-10 12:16:23 +0900

    feat: `PlayerUtil`

---
+++ src/main/java/net/starly/core/util/PlayerUtil.java
@@ -0,0 +1,15 @@
+package net.starly.core.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerUtil {
+    public static OfflinePlayer getOfflinePlayer(@NotNull String name) {
+        PreCondition.nonNull(name, "name은 null일 수 없습니다.");
+
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(name);
+        if (!(offlinePlayer.hasPlayedBefore() || offlinePlayer.isOnline())) return null;
+        return offlinePlayer;
+    }
+}
