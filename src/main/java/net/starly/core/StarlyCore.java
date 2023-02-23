3f884be843ffca9b5cf1c3e2f272f5de3419015f
yejunho10 <hyjcompany30@naver.com>
2023-02-23 21:01:07 +0900

    feat: `License System`

--- src/main/java/net/starly/core/StarlyCore.java
+++ src/main/java/net/starly/core/StarlyCore.java
@@ -2,12 +2,14 @@ package net.starly.core;
 
 import net.starly.core.bstats.Metrics;
 import net.starly.core.starlyplugin.StarlyPluginManager;
+import net.starly.core.util.LicenseUtil;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StarlyCore extends JavaPlugin {
     @Override
     public void onEnable() {
         new Metrics(this, 17172);
+        LicenseUtil.checkLicense(this, "831983e6-e4bf-4ca9-afd1-3cde282d269b");
     }
 
     @Override
