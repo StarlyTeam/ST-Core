5dcbb09047acd2ab9e4ba6e31c88290972ba0c54
yejunho10 <hyjcompany30@naver.com>
2023-03-12 16:58:45 +0900

    fix: Bugs

--- src/main/java/net/starly/core/util/InventoryUtil.java
+++ src/main/java/net/starly/core/util/InventoryUtil.java
@@ -11,7 +11,10 @@ import java.util.stream.Collectors;
 public class InventoryUtil {
     public static Integer getSpace(Inventory inventory) {
         int filled = (int) Arrays.stream(inventory.getContents()).filter(Objects::nonNull).count();
-        if (inventory instanceof PlayerInventory) if (filled > 36) filled = 36;
+        if (inventory instanceof PlayerInventory) {
+            filled -= Arrays.stream(((PlayerInventory) inventory).getArmorContents()).filter(Objects::nonNull).count();
+            if (((PlayerInventory) inventory).getItemInOffHand().getType() != Material.AIR) filled--;
+        }
         return inventory.getSize() - filled;
     }
 }
