bec3be1cb508b7c8c94e7dfe926a99f977ecdf49
yejunho10 <hyjcompany30@naver.com>
2023-03-19 11:33:20 +0900

    modify: message

--- src/main/java/net/starly/core/jb/context/MessageContext.java
+++ src/main/java/net/starly/core/jb/context/MessageContext.java
@@ -2,8 +2,8 @@ package net.starly.core.jb.context;
 
 public class MessageContext {
 
-    public static final String COMMAND_INVALID_ARGUMENT_SIZE = "§c올바른 명령어가 아닙니다.";
-    public static final String COMMAND_NOT_NULL_ARGUMENT = "§c반드시 %s 를 입력해야 합니다.";
-    public static final String COMMAND_PERMISSION_ERROR = "§c명령을 실행 할 권한이 없습니다.";
+    public static final String COMMAND_INVALID_ARGUMENT_SIZE = "§c잘못된 명령어입니다.";
+    public static final String COMMAND_NOT_NULL_ARGUMENT = "§c%s 를 입력해주세요.";
+    public static final String COMMAND_PERMISSION_ERROR = "§c당신은 명령을 실행할 권한이 없습니다.";
 
 }
