dd2da3f0931f77ef3bb9ef5c2a7c0bae712b49fb
yejunho10 <hyjcompany30@naver.com>
2023-03-31 21:47:34 +0900

    refactor: `Set` to `STSet`

--- src/main/java/net/starly/core/starlyplugin/StarlyPluginManager.java
+++ src/main/java/net/starly/core/starlyplugin/StarlyPluginManager.java
@@ -1,37 +1,45 @@
 package net.starly.core.starlyplugin;
 
 import net.starly.core.starlyplugin.impl.StarlyPlugin;
+import net.starly.core.util.collection.STSet;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 
 public class StarlyPluginManager {
-    private static final Map<JavaPlugin, Set<StarlyPlugin>> starlyPlugins = new HashMap<>();
+    private static final Map<JavaPlugin, STSet<StarlyPlugin>> starlyPlugins = new HashMap<>();
 
     public static void registerStarlyPlugin(JavaPlugin plugin, Class<? extends StarlyPlugin> clazz) {
-        StarlyPlugin instance;
+        StarlyPlugin instance = null;
         try {
             instance = clazz.newInstance();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("StarlyPlugin 객체를 생성하는 도중 오류가 발생했습니다.");
+        } catch (IllegalAccessException ignored) {
+            new IllegalAccessException(clazz.getSimpleName() + "의 객체를 생성할 수 없습니다.").printStackTrace();
+            return;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
         }
 
         try {
             instance.onRegister();
-        } catch (NoSuchMethodError e) {
-            throw new IllegalArgumentException("StarlyPlugin를 상속받지 않은 객체입니다.");
+        } catch (NoSuchMethodError ignored) {
+            new IllegalArgumentException(clazz.getSimpleName() + "에 onRegister 메소드가 존재하지 않습니다.").printStackTrace();
+            return;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
         }
 
-        Set<StarlyPlugin> classes = starlyPlugins.get(plugin);
+        STSet<StarlyPlugin> classes = new STSet<>(starlyPlugins.get(plugin));
         classes.add(instance);
 
         starlyPlugins.put(plugin, classes);
     }
 
-    public static void registerStarlyPlugin(JavaPlugin plugin, Set<Class<? extends StarlyPlugin>> classes) {
+    public static void registerStarlyPlugin(JavaPlugin plugin, STSet<Class<? extends StarlyPlugin>> classes) {
         classes.forEach(clazz -> {
             try {
                 registerStarlyPlugin(plugin, clazz);
@@ -46,7 +54,7 @@ public class StarlyPluginManager {
     }
 
     public static void unRegisterStarlyPlugin(JavaPlugin plugin) {
-        Set<StarlyPlugin> instances = starlyPlugins.get(plugin);
+        STSet<StarlyPlugin> instances = starlyPlugins.get(plugin);
         instances.forEach(StarlyPlugin::onUnRegister);
 
         starlyPlugins.remove(plugin);
