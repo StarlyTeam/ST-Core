0327667855ffa1da07a11e6df343598ff23e82d3
yejunho10 <hyjcompany30@naver.com>
2023-03-31 22:19:24 +0900

    fix: Bugs

--- src/main/java/net/starly/core/util/RegistryUtil.java
+++ src/main/java/net/starly/core/util/RegistryUtil.java
@@ -2,16 +2,12 @@ package net.starly.core.util;
 
 import net.starly.core.starlyplugin.StarlyPluginManager;
 import net.starly.core.starlyplugin.impl.StarlyPlugin;
+import net.starly.core.util.collection.STSet;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
 
-import java.util.Set;
-
 public class RegistryUtil {
-    public static void registerSubPlugin(JavaPlugin plugin, String packageName) {
-        Reflections reflections = new Reflections(packageName);
-        Set<Class<? extends StarlyPlugin>> clazz = reflections.getSubTypesOf(StarlyPlugin.class);
-
-        StarlyPluginManager.registerStarlyPlugin(plugin, new Reflections(packageName).getSubTypesOf(StarlyPlugin.class));
+    public static void registerSubPlugins(JavaPlugin plugin, String packageName) {
+        StarlyPluginManager.registerStarlyPlugin(plugin, new STSet<>(new Reflections(packageName).getSubTypesOf(StarlyPlugin.class)));
     }
 }
