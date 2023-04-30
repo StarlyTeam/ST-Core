805296325fb5cab9d94a0e9c69b30cb785dda767
yejunho10 <hyjcompany30@naver.com>
2023-04-30 09:47:53 +0900

    fix: Bugs

--- src/main/java/net/starly/core/jb/version/nms/VersionController.java
+++ src/main/java/net/starly/core/jb/version/nms/VersionController.java
@@ -6,32 +6,31 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class VersionController {
 
     public enum Version {
-        v1_12_R1("1.12", false),
+        v1_12_R1("1.12-R0.1 | 1.12.1-R0.1 | 1.12.2-R0.1", false),
 
-        v1_13_R1("1.13-R0.1", true),
-        v1_13_R2("1.13", true),
+        v1_13_R1("1.13-R0.1 | 1.13.1-R0.1 | 1.13-R0.1", true),
+        v1_13_R2("1.13.2-R0.1", true),
 
-        v1_14_R1("1.14-R0.1", true),
+        v1_14_R1("1.14-R0.1 | 1.14.1-R0.1 | 1.14.2-R0.1 | 1.14.3-R0.1 | 1.14.4-R0.1", true),
 
-        v1_15_R1("1.15-R0.1", true),
+        v1_15_R1("1.15-R0.1 | 1.15.1-R0.1 | 1.15.2-R0.1", true),
 
-        v1_16_R3("1.16.5-R0.1|1.16.4-R0.1", true),
+        v1_16_R1("1.16-R0.1 | 1.16.1-R0.1 | 1.16.2-R0.1", true),
         v1_16_R2("1.16.3-R0.1", true),
-        v1_16_R1("1.16.2-R0.1|1.16.1-R0.1|1.16-R0.1", true),
+        v1_16_R3("1.16.4-R0.1 | 1.16.5-R0.1", true),
 
-        v1_17_R1("1.17-R0.1", true),
+        v1_17_R1("1.17-R0.1 | 1.17.1-R0.1", true),
 
-        v1_18_R2("1.18-R0.2", true),
-        v1_18_R1("1.18", true),
+        v1_18_R1("1.18-R0.1 | 1.18.1-R0.1", true),
+        v1_18_R2("1.18.2-R0.1", true),
 
-        v1_19_R3("1.19.4-R0.1", true),
+        v1_19_R1("1.19-R0.1 | 1.19.1-R0.1 | 1.19.2-R0.1", true),
         v1_19_R2("1.19.3-R0.1", true),
-        v1_19_R1("1.19", true);
+        v1_19_R3("1.19.4-R0.1", true);
 
         private final String v;
         public final String version = name();
@@ -71,7 +70,7 @@ public class VersionController {
 
     private Version checkVersions(Server server) throws UnSupportedVersionException {
         System.out.println(server.getBukkitVersion());
-        Optional<Version> versionFilter = Arrays.stream(Version.values()).filter(it -> Arrays.stream(it.v.split("\\|")).anyMatch(v -> server.getBukkitVersion().contains(v))).findFirst();
+        Optional<Version> versionFilter = Arrays.stream(Version.values()).filter(it -> Arrays.stream(it.v.split(" \\| ")).anyMatch(v -> server.getBukkitVersion().contains(v))).findFirst();
         if (versionFilter.isPresent()) return versionFilter.get();
         else throw new UnSupportedVersionException(server.getVersion());
     }
