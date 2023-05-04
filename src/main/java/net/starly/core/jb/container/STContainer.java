85842e8a052a08c885f1e99b033b2940778f00f2
yejunho10 <hyjcompany30@naver.com>
2023-05-04 22:18:12 +0900

    fix: Bugs

--- src/main/java/net/starly/core/jb/container/STContainer.java
+++ src/main/java/net/starly/core/jb/container/STContainer.java
@@ -102,18 +102,9 @@ public abstract class STContainer implements InventoryHolder {
             viewer = player;
             initializingInventory(inventory);
             plugin.getServer().getScheduler().runTaskLater(plugin, ()->{
-                InventoryView openInventory = player.getOpenInventory();
-                if (!openInventory.getType().equals(InventoryType.PLAYER)
-                        && !openInventory.getType().equals(InventoryType.CREATIVE)
-                        && !openInventory.getType().equals(InventoryType.CRAFTING)
-                ) {
-                    player.closeInventory();
-                    plugin.getServer().getScheduler().runTaskLater(plugin, () ->  open(player), 1L);
-                } else {
-                    openedInitializing();
-                    player.openInventory(inventory);
-                    registerPlayer(player);
-                }
+                openedInitializing();
+                player.openInventory(inventory);
+                registerPlayer(player);
             }, 1L);
         } catch (Exception e) { player.closeInventory(); }
     }
