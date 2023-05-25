d525ff28042fc7cc42e320a3e00c210b5922034a
yejunho10 <hyjcompany30@naver.com>
2023-05-25 17:16:41 +0900

    refactor: style code

--- src/main/java/net/starly/core/util/PreCondition.java
+++ src/main/java/net/starly/core/util/PreCondition.java
@@ -1,6 +1,9 @@
 package net.starly.core.util;
 
 public class PreCondition {
+
+    private PreCondition() {}
+
     public static void nonNull(Object object, String message) {
         if (object == null) throw new NullPointerException(message);
     }
